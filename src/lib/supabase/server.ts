0509c591a4866d8fca25abc47b9a8670617e0b8d
books-admin <n0548454351@gmail.com>
2026-04-20 20:59:53 +0300

    fix cookiesToSet type

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -9,7 +9,7 @@ export async function createClient() {
     {
       cookies: {
         getAll() { return cookieStore.getAll(); },
-        setAll(cookiesToSet: any) {
+      setAll(cookiesToSet: any) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
